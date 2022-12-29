ef09ee8df2e319284fdb1524891d277f301a1838
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-12-29 13:04:29 +0530

    Added: Ability to check Employee Present or Absent

---
+++ EmpWage.js
@@ -0,0 +1,11 @@
+// UC1 - Ability to check Employee Present or Absent
+
+function getAttendance() {
+    let checkStatus =  Math.floor(Math.random() * 10) %  2;
+    if(checkStatus == 0)
+        console.log("Employee is absent");
+    else
+        console.log("Employee is present");
+    }
+
+    getAttendance();
